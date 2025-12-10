7687c341a649bcb557eb569b195ff98a0341b47b
Karthik Mokara <mokarakarthik.22.ece@anits.edu.in>
2025-12-10 21:56:05 +0530

    implement express-rate-limit with global limiter

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,11 +5,19 @@ import {
   refreshTokenController,
   logout
 } from "../controllers/auth.controller.js";
+import rateLimit from "express-rate-limit";
 
 const router = express.Router();
 
 router.post("/signup", signup);
-router.post("/login", login);
+
+const loginLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 5,
+  message: { msg: "Too many login attempts, try again later." },
+});
+
+router.post("/login", loginLimiter, login);
 router.post("/token", refreshTokenController);
 router.post("/logout", logout);
 
--- server.js
+++ server.js
@@ -3,11 +3,22 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import { initDB } from "./db/database.js";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(helmet(
+  {contentSecurityPolicy:false}
+));
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100, // limit each IP to 100 requests/min
+  message: { msg: "Too many requests, slow down!" },
+});
+app.use(limiter);
 
 // Init DB
 await initDB();
